ca9a933723902ea92057cb67868bb7960b0a26ce
shailesh mourya <ssm.2000mourya@gmail.com>
2024-02-08 14:57:26 +0530

    v5 created test_question

---
+++ test_question.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
+    <!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+
+        <!-- place navbar here -->
+        <?php 
+        include('header.php');
+        
+        ?>
+    </header>
+    <main>
+        <!-- question model -->
+
+        <div class="m-1 m-lg-3" style="background-color:white">
+            <p class="m-3 sticky-top ">Time left: <span id="timer" class="text-danger"></span></p>
+            <script>
+                var timeLeft = 300; // 5 minutes countdown
+                function startTimer() {
+                    var timerInterval = setInterval(function () {
+                        timeLeft--;
+                        document.getElementById('timer').innerText = timeLeft + 's';
+                        if (timeLeft <= 0) {
+                            clearInterval(timerInterval);
+                            document.getElementById('testForm').submit(); // Automatically submit the form when time is up
+                        }
+                    }, 1000);
+                }
+                window.onload = startTimer;
+            </script>
+
+            <?php 
+        $topic=1;
+        
+        include('config.php');
+        $sql="SELECT * FROM `gk_test` WHERE `topic_name`='solar system';";
+        $result=mysqli_query($conn,$sql);
+        $rows=mysqli_fetch_assoc($result);
+        echo "<h1 class='h5 text-center mt-2'>Test on topic solar system</h1>";
+        
+
+        if (mysqli_num_rows($result)>0) {
+
+         echo "<div class='container mt-2'>";
+         echo "<form action='submit_test.php' method='post'><ol type='1'>";
+
+          while ($rows=mysqli_fetch_assoc($result)) {
+            echo "
+            <hr />";
+            //echo "<p>{$rows['gk_question']}</p>";
+            echo "<li><p>{$rows['gk_question']}</p></li>";
+            echo "<ol type='A'>
+            <li><input class='form-check-input  mx-1' name='r{$rows['id']}' type='radio' value='' id='checkbox1'>{$rows['gk_option1']}</li>
+            <li><input class='form-check-input  mx-1' name='r{$rows['id']}' type='radio' value='' id='checkbox1'>{$rows['gk_option2']}</li>
+            <li><input class='form-check-input  mx-1' name='r{$rows['id']}' type='radio' value='' id='checkbox1'>{$rows['gk_option3']}</li>
+            <li><input class='form-check-input  mx-1' name='r{$rows['id']}' type='radio' value='' id='checkbox1'>{$rows['gk_option4']}</li>
+          </ol>";
+         
+        
+            
+
+          }
+          echo "</ol></div>";
+
+
+        }
+        
+        
+        
+        
+        
+        ?>
+            <div class=' text-center mb-4'>
+                <button class="btn mb-4" style="background-color: #f7eeee" type="submit">
+                    <i class="bi bi-eye-fill"></i> Submit
+                </button>
+            </div>
+            </form>
+        </div>
+
+
+
+        <!-- sidebar -->
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample"
+            aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">TLearner</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active  href=" #" aria-current="page">
+                            <i class="bi bi-house-door-fill text-primary "></i> Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-person-fill text-primary"></i> About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-box-arrow-in-right text-primary"></i> Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-r-circle-fill text-primary"></i> Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="bi bi-telephone-fill text-primary"></i> Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="bi bi-share-fill text-primary"></i> Share
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
